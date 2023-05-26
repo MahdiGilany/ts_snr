9fddd7e430c83b172f73f8c50428f45102375969
mahdigilany <abbasgilanin199%>
2023-05-26 17:25:05 -0400

    added deeptime

--- scripts/gaussian_noise_check.sh
+++ scripts/gaussian_noise_check.sh
@@ -4,10 +4,22 @@ for noise_std in 0 10 50 100 200
 do
  for seed in {0..4}
  do
- echo "seed ${seed} and noise std ${noise_std} main experiment exp_default"
- python main.py experiment=exp_default seed=${seed}\
+ echo "seed ${seed} and noise std ${noise_std} main experiment nbeats"
+ python main.py experiment=nbeats seed=${seed}\
  data.noise_std=${noise_std}\
  name="nbeats_airpassenger_noisestd${noise_std}_seed${seed}"\
- logger.wandb.group=noise_std${noise_std}_v2
+ logger.wandb.group=nbeats_noise_std${noise_std}
  done
 done
+
+# for noise_std in 0 10 50 100 200
+# do
+#  for seed in {0..4}
+#  do
+#  echo "seed ${seed} and noise std ${noise_std} main experiment deeptime"
+#  python main.py experiment=deeptime seed=${seed}\
+#  data.noise_std=${noise_std}\
+#  name="deeptime_airpassenger_noisestd${noise_std}_seed${seed}"\
+#  logger.wandb.group=deeptime_noise_std${noise_std}
+#  done
+# done
