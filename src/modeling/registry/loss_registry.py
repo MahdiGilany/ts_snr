1d4e88ffd5236d98dff3e168727197408a9d1143
mahdigilany <abbasgilanin199%>
2023-05-30 15:46:25 -0400

    loss functions to use in future

--- src/modeling/registry/loss_registry.py
+++ src/modeling/registry/loss_registry.py
@@ -53,3 +53,12 @@ def crossentropy_loss(num_classes=2):
     
     
     return wrap_loss    
+
+
+# def get_loss_fn(loss_name: str,
+#                 delta: Optional[float] = 1.0,
+#                 beta: Optional[float] = 1.0) -> Callable:
+#     return {'mse': F.mse_loss,
+#             'mae': F.l1_loss,
+#             'huber': partial(F.huber_loss, delta=delta),
+#             'smooth_l1': partial(F.smooth_l1_loss, beta=beta)}[loss_name]
