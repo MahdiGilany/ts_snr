613fe02f3ee94acc1035a31d7516caeeea4059cf
abbasgln <mahdi.gilany@borealisai.com>
2023-08-03 20:54:34 +0000

    minor update

--- scripts/skynet_noise_without_loop.sh
+++ scripts/skynet_noise_without_loop.sh
@@ -3,7 +3,7 @@
 #SBATCH --mem=25G
 #SBATCH --gres=gpu:1
 #SBATCH --time=1-01:00:00
-#SBATCH --exclude=compute1080ti06,compute1080ti08,compute1080ti10
+#SBATCH --exclude=compute1080ti06,compute1080ti07,compute1080ti08,compute1080ti10
 #SBATCH -c 6 
 #SBATCH -o /home/abbasgln/code/ts_snr/slurm_logs/%J.out
 #SBATCH -e /home/abbasgln/code/ts_snr/slurm_logs/%J.err 
@@ -36,6 +36,11 @@ epochs=100
 lr=0.001
 patience=10
 dataset_name="etth2"
+
+# only for crypto
+crypto_name="Bitcoin"
+prct_rows_to_load=0.1
+
 noise_type="gaussian"
 noise_std=0
 target_series_index=-1
@@ -81,6 +86,8 @@ python main.py name=$name\
             model.output_chunk_length=$output_chunk_length\
             model.optimizer_kwargs.lr=$lr\
             data.dataset_name=$dataset_name\
+            +data.crypto_name=$crypto_name\
+            +data.prct_rows_to_load=$prct_rows_to_load\
             data.noise_type=$noise_type\
             data.noise_std=$noise_std\
             data.target_series_index=$target_series_index\
